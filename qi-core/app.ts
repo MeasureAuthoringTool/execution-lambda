2adaa57d98eeb8ede7cdee732cbba6e95b6b9321
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-06-20 15:32:11 -0500

    remove power tool logger for build issue on dev

--- qi-core/app.ts
+++ qi-core/app.ts
@@ -3,9 +3,8 @@ import { CalculationOutput } from 'fqm-execution/build/types/Calculator';
 import { Measure, TestCase } from '@madie/madie-models';
 import { Bundle, ValueSet } from 'fhir/r4';
 import calculateTestCases from './api/CalculationService';
-import { Logger } from '@aws-lambda-powertools/logger';
 
-const logger = new Logger();
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -17,7 +16,7 @@ const logger = new Logger();
  */
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    logger.info('Entering lambdaHandler...');
+    console.log('Entering lambdaHandler...');
     console.log('Remaining time: ', context.getRemainingTimeInMillis());
     console.log('Function name: ', context.functionName);
     let body;
@@ -31,7 +30,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Contex
         body = event.body;
         const input = JSON.parse(body);
         const measure: Measure = input.measure;
-        logger.info('measure id = ' + measure?.id);
+        console.log('measure id = ' + measure?.id);
         const testCases: TestCase[] = input.testCases;
         const measureBundle: Bundle = input.measureBundle;
         const valueSets: ValueSet[] = input.valueSets;
